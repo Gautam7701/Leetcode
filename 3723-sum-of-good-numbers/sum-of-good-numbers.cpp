95085652389210b6e36a7f8705ccc6f77976c4c5
Gautam Gupta <141417348+Gautam7701@users.noreply.github.com>
2025-03-25 15:59:39 +0530

    Time: 0 ms (100.00%) | Memory: 27.2 MB (60.15%) - LeetSync

--- 3723-sum-of-good-numbers/sum-of-good-numbers.cpp
+++ 3723-sum-of-good-numbers/sum-of-good-numbers.cpp
@@ -1,20 +1,32 @@
 class Solution {
 public:
     int sumOfGoodNumbers(vector<int>& nums, int k) {
-        
-        int ans=0;
-        for(int i =0;i<nums.size();i++){
-            bool isGood=true;
-            if(i-k>=0 &&nums[i]<=nums[i-k]){
-                isGood = false;;
+        int sum =0;
+        int n=nums.size();
+        for(int i =0;i<n;i++){
+            int fd=i-k;
+            int bd=i+k;
+            int temp=0;
+
+            if(fd>=0 && bd<n){
+                if(nums[i]>nums[fd] && nums[i]>nums[bd]){
+                    sum+=nums[i];
+                }
             }
-            if(i+k<nums.size() && nums[i]<=nums[i+k]){
-                isGood=false;;
+            else if(fd<0 && bd<n){
+                if(nums[i]>nums[bd]){
+                    sum+=nums[i];
+                }
             }
-            if(isGood){
-                ans+=nums[i];
+            else if(fd>=0 && bd>=n){
+                if(nums[i]>nums[fd]){
+                    sum+=nums[i];
+                }
+            }
+            else{
+                sum+=nums[i];
             }
         }
-        return ans;
+        return sum;
     }
 };

94078bb298cc986dec079bfacdd83b51d038c18c
Gautam Gupta <141417348+Gautam7701@users.noreply.github.com>
2025-04-24 16:09:34 +0530

    Time: 0 ms (100.00%) | Memory: 26.2 MB (33.84%) - LeetSync

--- 540-single-element-in-a-sorted-array/single-element-in-a-sorted-array.cpp
+++ 540-single-element-in-a-sorted-array/single-element-in-a-sorted-array.cpp
@@ -8,12 +8,12 @@ public:
         while(st<=end){
             int mid = st+(end-st)/2;
             if(mid==0 && arr[0]!=arr[1]) return arr[mid];
-            if(mid==n-1 && arr[n-1]!=arr[n-2]) return arr[mid];
+            if(mid == n-1 && arr[n-1]!= arr[n-2]) return arr[mid];
 
             if(arr[mid-1]!=arr[mid] && arr[mid]!=arr[mid+1]){
                 return arr[mid];
             }
-            if(mid %2==0){
+            if(mid%2==0){
                 if(arr[mid-1]==arr[mid]){
                     end = mid-1;
                 }

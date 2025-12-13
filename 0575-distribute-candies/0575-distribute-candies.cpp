d2e602a4eb9dd28b4f89a69cea68ddc4199696d5
Gautam Gupta <141417348+Gautam7701@users.noreply.github.com>
2025-12-13 14:41:37 +0530

    Time: 44 ms (90.9%), Space: 85.4 MB (99.42%) - LeetHub

---
+++ 0575-distribute-candies/0575-distribute-candies.cpp
@@ -0,0 +1,14 @@
+class Solution {
+public:
+    int distributeCandies(vector<int>& candyType) {
+        sort(candyType.begin(),candyType.end());
+        int n = candyType.size();
+        int unique=1;
+        for(int i =1;i<n;i++){
+            if(candyType[i]!=candyType[i-1]){
+                unique++;
+            }
+        }
+    return min(unique,n/2);
+    }
+};
